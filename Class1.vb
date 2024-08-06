fc7e5530d9863bc385ad229faf3b7556f44299e0
yulianaAlias <karensalamanca90@gmail.com>
2024-08-06 19:40:05 -0300

    tarea entregada karen Alias

---
+++ Class1.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.VisualBasic
+
+
+Module Program
+    Sub Main()
+        Dim L As Double
+        Dim F As Double
+        Dim XL As Double
+        Console.WriteLine("ingrese el valor de la inductancia")
+        L = Console.ReadLine()
+        Console.WriteLine("ingrese el valor de la frecuencia")
+        F = Console.ReadLine()
+        XL = 2 * 3.14 * F * L
+        Console.WriteLine("la reactancia inductiva es," & XL)
+        Console.ReadLine()
+    End Sub
+End Module
